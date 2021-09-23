9393e0586732b15ee7719e214d7c52179f9d004d
Tej Kumar Rai <t_plasmid@yahoo.com>
2021-09-23 13:42:12 +0100

    LABs 3.4.1.6, 3.4.1.13 and 3.6.1.9 updated

--- Python_Essentials_1_LABS-Tej/chapter2.py
+++ Python_Essentials_1_LABS-Tej/chapter2.py
@@ -1,26 +1,25 @@
 # Author: Sgt Tej Kumar Rai
 # Date Modified: 22/09/2021
+# Date Last Modified: 23/09/2021
 # Description: This source file contains all the labs from Chapter 2 of Pthon Essentils 1 module.
-
-# ******************************************************************* Start: Chapter 2 ******************************************************************* #
 # The topics covered within this chapter are data types, variables, basic input-output operations and basic operators.
 
-# 2.1.1.6 LAB: The print() function *********** (Snippet 1) ***********
-# Start: This snippet invokes the print function using quotes and parenthesis
+# 2.1.1.6 LAB: The print() function *********** (LAB 1) ***********
+# Description: This LAB invokes the print function using quotes and parenthesis
 print("Hello, Python!") # Uses double quotes
 print("Tej") # Prints my name
 print('Tej') # Uses single quotes
 print() # Print a new line
 
-# 2.1.1.18 LAB: The print() function *********** (Snippet 2) ***********
-# Start:  This snippet uses keyword arguments sep and end of the print function
+# 2.1.1.18 LAB: The print() function *********** (LAB 2) ***********
+# Description:  This LAB uses keyword arguments sep and end of the print function
 print("Programming","Essentials","in", sep="***", end="...") # Keyword arguments sep and end are used to generated the expected output
 print("Python") 
 print() # Print a new line
-# End
 
-# 2.1.1.19 LAB: Formatting the output *********** (Snippet 3) ***********
-# Start: The snippet formats the character output
+
+# 2.1.1.19 LAB: Formatting the output *********** (LAB 3) ***********
+# Description: The LAB formats the character output
 print("    *")
 print("   * *")
 print("  *   *")
@@ -50,16 +49,16 @@ print("    *       *    " * 2)
 print('    *       *    ' * 2) # Here quotes has been replaced with apostrophes to test its effect.
 print("    *********    " * 2)
 print() # Print a new line
-# End
 
-# 2.2.1.11 LAB: Python literals - strings *********** (Snippet 4) ***********
-# Start: This snippet make use of one-line print function to output three lines of outputs.
+
+# 2.2.1.11 LAB: Python literals - strings *********** (LAB 4) ***********
+# Description: This LAB make use of one-line print function to output three lines of outputs.
 print('"I\'m\n""learning""\n"""Python"""') # One-line print() function which outputs three lines of expected output.
 print() # Print a new line
-# End
 
-# 2.4.1.7 LAB: Variables *********** (Snippet 5) ***********
-# Start: This snippet makes use of variables to calculate the total number of apples.
+
+# 2.4.1.7 LAB: Variables *********** (LAB 5) ***********
+# Description: This LAB makes use of variables to calculate the total number of apples.
 john = 3 # John had three apples
 mary = 5 # Mary had five apples
 adam = 6 # Adam had six apples
@@ -67,10 +66,10 @@ print(john, mary, adam, sep=",") # It prints the number of apples each of them h
 total_apples = john + mary + adam # It calculates the total apple each of them had.
 print("Total number of apples:", total_apples) # Finally, it prints the total number of apples.
 print() # Print a new line
-# End
 
-# 2.4.1.9 LAB: Variables: a simple converter *********** (Snippet 6) ***********
-# Start: This snippet converts miles to kilometer and vice-versa.
+
+# 2.4.1.9 LAB: Variables: a simple converter *********** (LAB 6) ***********
+# Description: This LAB converts miles to kilometer and vice-versa.
 kilometers = 12.25
 miles = 7.38
 
@@ -80,19 +79,19 @@ kilometers_to_miles = kilometers / 1.61 # Converts kilometers to miles. Note: 1 
 print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
 print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
 print() # Print a new line
-# End
 
-# 2.4.1.10 LAB: Operators and expressions *********** (Snippet 7) ***********
-# Start: This snippet evaluates the expression and ouput the result to the terminal.
+
+# 2.4.1.10 LAB: Operators and expressions *********** (LAB 7) ***********
+# Description: This LAB evaluates the expression and ouput the result to the terminal.
 x =  -1 # Sample inputs are 0, 1 or -1
 x = float(x)
 y = (3 * x ** 3) - (2 * x ** 2) + (3 * x) - 1 # Here y = 3x^3 - 2x^2 + 3x - 1
 print("y =", y) # Expected outputs are -1.0, 3.0 and -9.0 respectively
 print() # Print a new line
-# End
 
-#2.5.1.2 LAB: Comments *********** (Snippet 8) ***********
-# Start: This snippet computes the number of seconds in a given number of hours
+
+#2.5.1.2 LAB: Comments *********** (LAB 8) ***********
+# Description: This LAB computes the number of seconds in a given number of hours
 hours = 2 # Number of hours
 seconds = 3600 # Number of seconds in 1 hour
 
@@ -100,10 +99,10 @@ print("Hours: ", hours) # Prints the number of hours
 print("Seconds in Hours: ", hours * seconds) # Prints the number of seconds in a given number of hours
 print("Goodbye") # Prints Goodbye
 print() # Print a new line
-# End
 
-# 2.6.1.9 LAB: Simple input and output *********** (Snippet 9) ***********
-# Start: This snippet evalueates the results of four basic arithmetic operations
+
+# 2.6.1.9 LAB: Simple input and output *********** (LAB 9) ***********
+# Description: This LAB evalueates the results of four basic arithmetic operations
 a = float(input("Enter the first value:")) # input a float value for variable a here
 b = float(input("Enter the second value:")) # input a float value for variable b here
 
@@ -113,18 +112,18 @@ print(str(a * b)) # output the result of multiplication here
 print(str(a / b)) # output the result of division here
 print("\nThat's all, folks!")
 print() # Print a new line
-# End
 
-# 2.6.1.10 LAB: Operators and expressions *********** (Snippet 10) ***********
-# Start: This snippet evaluates the expression below.
+
+# 2.6.1.10 LAB: Operators and expressions *********** (LAB 10) ***********
+# Description: This LAB evaluates the expression below.
 x = float(input("Enter value for x: ")) # Input a float value for variable a
 y = 1 / (x + 1 / (x + 1 / (x + 1 / x))) # Evaluates the expression on the right and stores the value on y
 print("y =", y) # Prints the evaluated value
 print() # Print a new line
-# End
 
-# 2.6.1.11 LAB: Operators and expressions *********** (Snippet 11) ***********
-# Start: This snippet evaluates the end time from the given time and duration
+
+# 2.6.1.11 LAB: Operators and expressions *********** (LAB 11) ***********
+# Description: This LAB evaluates the end time from the given time and duration
 hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
@@ -134,6 +133,3 @@ end_mins = (mins + dura) % 60 # Returns remainder which is always less than 60 m
 
 print(end_hour, end_mins, sep=":") # Prints output in the format hh:mm
 print() # Print a new line
-# End
-
-# ******************************************************************* End: Chapter 2 ******************************************************************* #
--- Python_Essentials_1_LABS-Tej/chapter3.py
+++ Python_Essentials_1_LABS-Tej/chapter3.py
@@ -1,28 +1,27 @@
 # Author: Sgt Tej Kumar Rai
-# Date Modified: 22/09/2021
+# Date Created: 22/09/2021
+# Date Last Modified: 23/09/2021
 # Description: This source file contains all the labs from Chapter 3 of Pthon Essentils 1 module.
+# # The topics covered within this chapter are Boolean Values, Conditional Execution, Loops, Lists and List Processing, Logical and Bitwise Operations
 
-# ******************************************************************* Start: Chapter 3 ******************************************************************* #
-# The topics covered within this chapter are Boolean Values, Conditional Execution, Loops, Lists and List Processing, Logical and Bitwise Operations
-
-# 3.1.1.4 LAB: Questions and answers *********** (Snippet 1) ***********
-# Start: This snippet uses a comparions operator to ouput False if n < 100 or True otherwise
+# 3.1.1.4 LAB: Questions and answers *********** (LAB  1) ***********
+# Description: This LAB  uses a comparions operator to ouput False if n < 100 or True otherwise
 n = int(input("Enter an integer:")) # Asks for an integer input
 print(n >= 100) # Prints False if n < 100 or True otherwise
 print() # Print a new line
-# End
 
-# 3.1.1.10 LAB: Comparison operators and conditional execution *********** (Snippet 2) ***********
-# Start: This snippet asks a user for the name of a houseplant and then generates output to the screen depending on the input.
+
+# 3.1.1.10 LAB: Comparison operators and conditional execution *********** (LAB  2) ***********
+# Description: This LAB  asks a user for the name of a houseplant and then generates output to the screen depending on the input.
 plant_name = input("Enter the name of an indoor houseplant:")
 if plant_name == "Spathiphyllum": print ("Yes - Spathiphyllum is the best plant ever!")
 elif plant_name == "spathiphyllum": print ("No, I want a big Spathiphyllum!")
 else: print("Spathiphyllum! Not [" + plant_name + "]!")
 print() # Print a new line
-# End
 
-# 3.1.1.11 LAB: Essentials of the if-else statement *********** (Snippet 3) ***********
-# Start: This snippet calculates the Personal Income Tax (PIT) which had to be paid once a year by the inhabitant of a land of milk and honey
+
+# 3.1.1.11 LAB: Essentials of the if-else statement *********** (LAB  3) ***********
+# Description: This LAB  calculates the Personal Income Tax (PIT) which had to be paid once a year by the inhabitant of a land of milk and honey
 income = float(input("Enter the annual income: "))
 
 if income <= 85_528: tax = (income * 0.18) - 556.02
@@ -33,10 +32,10 @@ if tax < 0: tax = 0.0 # If the calculated tax is less tahn zero, it only means n
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
 print() # Print a new line
-# End
 
-# 3.1.1.12 LAB: Essentials of the if-elif-else statement *********** (Snippet 4) ***********
-# Start: This snippet checks if the given year is a leap year or not
+
+# 3.1.1.12 LAB: Essentials of the if-elif-else statement *********** (LAB  4) ***********
+# Description: This LAB  checks if the given year is a leap year or not
 year = int(input("Enter a year: "))
 
 # Checks if the entered year falls into the Gregorian era
@@ -48,11 +47,11 @@ else:
     elif year % 400 != 0: print ("Common year")
     else: print ("Leap year")
 print() # Print a new line
-# End
+
 print("Test")
 
-# 3.2.1.3 LAB: Essentials of the while loop *********** (Snippet 5) ***********
-# Start: This snippet uses while loop to allow a user to guess the magician's secret number.
+# 3.2.1.3 LAB: Essentials of the while loop *********** (LAB  5) ***********
+# Description: This LAB  uses while loop to allow a user to guess the magician's secret number.
 secret_number = 777
 
 print(
@@ -74,10 +73,10 @@ while number != secret_number: # Check whether the number entered by the user is
 
 print("Well done, mubble! You are free now.") # If the number entered by the user matches the number picked by the magician, the user sees this message.
 print() # Print a new line
-# End:
 
-# 3.2.1.6 LAB: Essentials of the for loop *********** (Snippet 6) ***********
-# Start: This snippet uses the for loop to print five outputs, with each iteration representing 1 second. Finally, it prints the final message.
+
+# 3.2.1.6 LAB: Essentials of the for loop *********** (LAB  6) ***********
+# Description: This LAB  uses the for loop to print five outputs, with each iteration representing 1 second. Finally, it prints the final message.
 import time # imports the time module
 
 for count in range(1,6): # Assigns the control variable 'count' a number from 1 to 5 in turn
@@ -85,10 +84,10 @@ for count in range(1,6): # Assigns the control variable 'count' a number from 1 
     time.sleep(1) # Use time.sleep() method to pause for 1 second
 
 print("Ready or not, here I come!") # prints the final message.
-# End
 
-# 3.2.1.9 LAB: The break statement - Stuck in a loop *********** (Snippet 7) ***********
-# Start: This snippet uses while loop and break keyword to loop and continuously asks a user to enter a word unless the user neters "chupacabra".
+
+# 3.2.1.9 LAB: The break statement - Stuck in a loop *********** (LAB  7) ***********
+# Description: This LAB  uses while loop and break keyword to loop and continuously asks a user to enter a word unless the user neters "chupacabra".
 correct_word = "chupacabra" # Word needed to exit the loop below.
 user_word = "" # initialise user-word variable with an empty string
 
@@ -98,10 +97,10 @@ while True: # Loop indefinitely
     else:
         user_word = input("Enter a word:") # Asks the user to enter a word
 print("You've successfully left the loop.") # prints this message when the user successfuly exits the loop
-# End
 
-# 3.2.1.10 LAB: The continue statement - the Ugly Vowel Eater *********** (Snippet 8) ***********
-# Start: This snippet uses for loop and continue keyword to remove vowel from the word enterd by a user.
+
+# 3.2.1.10 LAB: The continue statement - the Ugly Vowel Eater *********** (LAB  8) ***********
+# Description: This LAB  uses for loop and continue keyword to remove vowel from the word enterd by a user.
 user_word = input("Enter a word:") # Prompt the user to enter a word and assigns it to the user_word variable.
 user_word = user_word.upper() # Use the string method upper() to convert the word entered by the user to upper case.
 for letter in user_word:
@@ -118,10 +117,10 @@ for letter in user_word:
         continue
     else:
         print(letter)
-# End
 
-# 3.2.1.11 LAB: The continue statement - the pretty Vowel Eater *********** (Snippet 9) ***********
-# Start: This snippet improves the last snippet by concatenating non-vowel letters into a long string during subsequent loop turns.
+
+# 3.2.1.11 LAB: The continue statement - the pretty Vowel Eater *********** (LAB  9) ***********
+# Description: This LAB  improves the last LAB  by concatenating non-vowel letters into a long string during subsequent loop turns.
 word_without_vowels = "" # Initialise the word_without_vowels variable with an empty string.
 
 user_word = input("Enter a word:") # Prompt the user to enter a word and assigns it to the user_word variable.
@@ -142,10 +141,10 @@ for letter in user_word:
         word_without_vowels += letter # Use concatenation operation to combine selected letters into a long string during subsequent loop turns.
 
 print(word_without_vowels) # Print the word assigned to word_without_vowels.
-# End
 
-# 3.2.1.14 LAB: Essentials of the while loop *********** (Snippet 10) ***********
-# Start: This snippet uses while loop to calculate the height of the pyramid that can be built using the given number of blocks 
+
+# 3.2.1.14 LAB: Essentials of the while loop *********** (LAB  10) ***********
+# Description: This LAB  uses while loop to calculate the height of the pyramid that can be built using the given number of blocks 
 # using one principle: each lower layer contains one block more than the layer above.
 blocks = int(input("Enter the number of blocks: ")) # Ask for the number of blocks the builders have
 height = 0 # Initialise height variable to 0
@@ -158,10 +157,10 @@ while blocks > 0: # Loop until blocks = 0
     else:
         break
 print("The height of the pyramid:", height) # Print the height of the pyramid that can be built using the given number of blocks.
-# End
 
-# 3.2.1.15 LAB: Collatz's hypothesis *********** (Snippet 11) ***********
-# Start: This snippet tests Collatz's hypothesis using the while loop.
+
+# 3.2.1.15 LAB: Collatz's hypothesis *********** (LAB  11) ***********
+# Description: This LAB  tests Collatz's hypothesis using the while loop.
 number = int(input("Enter a non-negative and non-zero integer number:")) # Ask for a natural number
 steps = 0 # Initialise steps variable to 0.
 if number > 0: 
@@ -175,5 +174,87 @@ if number > 0:
         steps += 1 # Increment steps variable by 1 evertime the loop is completed successfully.
         print(int(c0)) # Print all the intermediate values of c0
     print("steps =", steps) # Print the steps needed to reach the initial value of c0 to 1 using the algorithm.
-# End
-# ******************************************************************* End: Chapter 3 ******************************************************************* #
+
+
+# 3.4.1.6 LAB: The basics of lists *********** (LAB  12) ***********
+# Description: This LAB  modifies the existing list using basic instructions related to a list.
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+number = int(input("Enter a number:")) # Prompt the user for an integer number
+
+hat_list[2] = number # Replace the middle number of the list with the integer number entered by the user.
+del hat_list[4] # Remove the last element from the list.
+
+print(len(hat_list)) # Print the length of the existing list.
+print(hat_list) # Print the existing list.
+
+# 3.4.1.13 LAB: The basics of lists *********** (LAB  12) ***********
+# Description: This LAB  creates and modifies a simple list and then modifies the list using methods such as append() and insert() and del instruction.
+beatles = [] # Create an empty list named beatles.
+print("Step 1:", beatles)
+
+# Use the append() method to add the following members of the band to the list.
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+appended_once = False # Boolean variable to check if the list has already been appended once.
+for i in range(2): # Use for loop to iterate twice.   
+    while True: # Infinite while loop
+        band_member_name = input("Enter a band member name:") # Prompt user to input a band meember name.
+        if band_member_name == "Stu Sutcliffe" or band_member_name == "Pete Best": # If the band member name inputted by 
+                                                                                   # the user is either "Stu Sutcliffe" or "Pete Best" then execute the if block.
+            if not appended_once: # Check if the append operation has been carried out once. If not excute the if block.
+                beatles.append(band_member_name) # Append the band member name supplied by the user to the existing list.
+                appended_once = True # Since the existing list has been appended once change appended_once variable to True.
+                break # Break out of the while loop
+            else: # If the append operation has been carried out once before, execute the else block.
+                if beatles[3] != band_member_name: # If the band member name supplied by the user is the same as the one 
+                                                   # appended last time ask the user to input the name again otherwise execute the if block below
+                    beatles.append(band_member_name) # Append the band member name supplied by the user to the existing list.
+                    break
+print("Step 3:", beatles)
+
+del beatles[3] # Use the del instruction to remove the fourth item from the existing list.
+del beatles[3] # Use the del instruction to remove the fourth item from the existing list.
+print("Step 4:", beatles)
+
+beatles.insert(0, "Ringo Starr") # Use the insert method to add "Ringo Starr" to the beginning of the list.
+print("Step 5:", beatles)
+
+print("The Fab", len(beatles)) # Testing list legth
+
+# 3.6.1.9 LAB: Operating with lists - basics *********** (LAB  13) ***********
+# Description: This LAB  asks the user to input a list and generates another list with a unique element only.
+user_input = input("To use the default list, please Enter; otherwise, please enter a list. (Example: 1,22,22,3):") # Prompt the user to input a list. If the user does not provide a list the default list will be used.
+my_list = [] # Create an empty list to store a list
+if len(user_input) == 0: # Check if the user has provided a list or not.
+    my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9] # Default list to use if the user does not provide a list.
+else:
+    temp_list = [] # Create an empty list to store a temporary list.
+    temp = "" # Initialise the temp variable to an empty sting.
+    for character in user_input: # Loop through each character in user_input in turn.
+        if character != ",": temp += character # Concatinate character iteratively until ',' character is encountered.
+        else: # When ',' character is encountered append everything stored in temp variable to the temp_list.
+            temp_list.append(temp) 
+            temp = ""
+        if len(temp) > 0: temp_list.append(temp) # Once the for loop is completed if there are any data in the temp variable append the data to the temp_list.
+    my_list = temp_list[:] # Copy temp_list to my_list
+
+duplicate_item_present = True # Boolean variable to test if duplicate item is present.
+
+while duplicate_item_present: # While loop iterates infinitely until the condition is False.
+    for i in range(len(my_list)):
+        if i > 0: # if i > 0 copy all the items of my_list before after the my_list[i].
+            temp_list = my_list[:i] + my_list[i + 1:]
+        else: # Else copy all the items of my_list after the my_list[i].
+            temp_list = my_list[i + 1:]
+        if my_list[i] in temp_list: # if my_list[i] item is present in the temp_list created above delete my_list[i] and break the for loop
+            del my_list[i]
+            break
+        else: # Else check if i is equal to the length of my_list. If they are equal, it means that no duplicate item is present so the duplicate_item_present variable can now be set to False.
+            if i == len(my_list) - 1:
+                duplicate_item_present = False
+print("The list with unique elements only:")
+print(my_list)
